5925a75a69e56a0f64312e7d52ca79e961e92486
jasjeetkaur2211 <kaur.jasjeet2211@gmail.com>
2016-10-23 23:59:15 +0200

    Trying

---
+++ HelloWorld/src/HelloUser.java
@@ -0,0 +1,10 @@
+public class HelloUser {
+	public String userName;
+	// The name variable is assigned in the constructor.
+	   public HelloUser (String uName) {
+	      userName = uName;
+	   }
+	public void greetUser() {
+		System.out.println("Hello " + userName + "!");
+	}
+}
