7b14dd500775dcea49dc4ba128cd4f4d43bf61b2
jasjeetkaur2211 <kaur.jasjeet2211@gmail.com>
2016-10-24 20:33:26 +0200

    Update HelloWorld.java

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,42 +1,18 @@
-import java.io.*;
-
+/**
+ * 
+ */
 
 /**
  * @author Mohsen
  *
  */
-
-/**
- * @modified Jasjeet
- */
-
 public class HelloWorld {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		/*Initializing the newUser object with a Dummy userName */
-		HelloUser newUser = new HelloUser("Dummy");
-		
 		System.out.println("Hello ASE2016 - how are you? :)");
-		
-		/* try .. catch block to handle IO Exception */
-		try {
-			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-			System.out.print("Please enter your name: ");
-			
-			/* Reading User Input for name */
-			newUser.userName =	br.readLine();
-			
-			/* Calling the class function to print greeting message */
-			newUser.greetUser();
-		}
-		catch (IOException e)
-		{
-			e.printStackTrace();
-		}
 	}
 
 }
-
